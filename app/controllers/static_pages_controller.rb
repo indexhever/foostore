5c1ee4d8ae5dcaadb98536491739e658068abe50
heverton <heverton.sarah@gmail.com>
2017-04-05 22:49:57 -0300

    add product search

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
-  def home
-  	@products = Product.paginate(page: params[:page])
-  end
+	#show all products on home page or product searched
+	def home
+		@search_term = params[:looking_for]
+		@products = @search_term ? Product.where(name: @search_term).paginate(page: params[:page]) : Product.paginate(page: params[:page])
+	end
 end
