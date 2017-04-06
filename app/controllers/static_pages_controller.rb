d1ed31aa6e44f23d2a20bbd927f185c359da2410
heverton <heverton.sarah@gmail.com>
2017-04-06 01:14:56 -0300

    add User model and control

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
 	#show all products on home page or product searched
 	def home
+		User.create(name: "Admin User", email: "admin@nuuvem.com", password: "admin123", password_confirmation: "admin")
 		@search_term = params[:looking_for]
 		@products = @search_term ? Product.where(name: @search_term).paginate(page: params[:page]) : Product.paginate(page: params[:page])
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404180425) do
+ActiveRecord::Schema.define(version: 20170406031624) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -23,5 +23,15 @@ ActiveRecord::Schema.define(version: 20170404180425) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
