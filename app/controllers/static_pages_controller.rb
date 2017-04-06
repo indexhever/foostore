e75ae1bec07a321c3609ea1d322edd2d14c7731a
heverton <heverton.sarah@gmail.com>
2017-04-06 01:16:33 -0300

    fix user criation bug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 class StaticPagesController < ApplicationController
 	#show all products on home page or product searched
 	def home
-		User.create(name: "Admin User", email: "admin@nuuvem.com", password: "admin123", password_confirmation: "admin")
 		@search_term = params[:looking_for]
 		@products = @search_term ? Product.where(name: @search_term).paginate(page: params[:page]) : Product.paginate(page: params[:page])
 	end
