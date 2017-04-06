18a455e6ec563aaf79a27f9c2a85004f998c543c
heverton <heverton.sarah@gmail.com>
2017-04-06 15:11:11 -0300

    add login and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   	before_action :set_locale
 	protect_from_forgery with: :exception
+	include SessionsHelper
 
 	def set_locale
-		I18n.locale = params[:locale] || I18n.default_locale
+		#I18n.locale = params[:locale] || I18n.default_locale
+		# get locale setted on sessions_helper
+		I18n.locale = get_locale || I18n.default_locale
 	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,28 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+	if user && user.authenticate(params[:session][:password])
+		# Log the user in and redirect to the home page.
+		log_in user
+		redirect_to root_path
+	else
+		# Create an error message.
+		flash.now[:danger] = 'Invalid email/password combination'
+		render 'new'
+	end
+  end
+
+  def destroy
+  	log_out
+	redirect_to root_url
+  end
+
+  # set current website language
+  def localize
+  	define_locale params[:language]
+  	redirect_to :back
+  end
 end
